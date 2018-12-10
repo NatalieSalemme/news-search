a2d61e8132160b8083ebd8377091562a17405b35
Natalie Salemme <NatalieSalemme@gmail.com>
2018-12-10 00:36:03 -0800

    adding media queries to careers page

--- src/components/FooterLink/Careers.js
+++ src/components/FooterLink/Careers.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import Menu from '../Menu';
 import Footer from '../Footer';
-import { Button } from 'semantic-ui-react';
+import { Button, Image } from 'semantic-ui-react';
 
 class Careers extends React.Component {
   componentDidMount() {
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
   }
   render() {
     return (
       <div>
         <Menu />
         <div className="careers-container">
-
-        <img
-          className="career-img"
-          src="http://i67.tinypic.com/2wgbrxx.png"
-          alt="Careers thumbnail"
-        />
+          <Image
+            fluid
+            className="career-img"
+            src="http://i67.tinypic.com/2wgbrxx.png"
+            alt="Careers thumbnail"
+          />
+          <div className="career-action">
+            <Button className="apply-button">Apply Now</Button>
+          </div>
         </div>
 
         <Footer />
       </div>
-    )
+    );
   }
 }
-
 
 export default Careers;
