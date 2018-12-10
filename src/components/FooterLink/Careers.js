8b93e8a4b8e6fde3e3682cda9d5617202af18d19
Natalie Salemme <NatalieSalemme@gmail.com>
2018-12-09 23:59:35 -0800

    adding photo to careers page

--- src/components/FooterLink/Careers.js
+++ src/components/FooterLink/Careers.js
@@ -11,20 +11,14 @@ class Careers extends React.Component {
     return (
       <div>
         <Menu />
-          <div id="pointer">
-            <h3 className="banner-text">Employment Opportunities</h3>
-          </div>
-          <div className="career-action">
-            <h1>{'Join Our Team'.toUpperCase()}</h1>
-            <p>Be part of the News family and start your journalism career today!</p>
-            <Button className="apply-button">Apply Now</Button>
-          </div>
+        <div className="careers-container">
+
         <img
           className="career-img"
-          src="https://images.unsplash.com/photo-1525857332689-4b3110c2d3f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=928&q=80"
+          src="http://i67.tinypic.com/2wgbrxx.png"
           alt="Careers thumbnail"
         />
-
+        </div>
 
         <Footer />
       </div>
